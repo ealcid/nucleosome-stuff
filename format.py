d588b8585dd9ad56a3051179b9eff2761c067546
ealcid <Projects>
2014-05-19 12:28:41 -0700

    changes output of geneTrack

--- format.py
+++ format.py
@@ -1,45 +1,54 @@
 import sys
 
-chromDict = {"chr01":"I",
-			"chr02":"II",
-			"chr03":"III", 
-			"chr04":"IV",
-			"chr05":"V",
-			"chr06":"VI",
-			"chr07":"VII",
-			"chr08":"VIII",
-			"chr09":"IX",
-			"chr10":"X",
-			"chr11":"XI",
-			"chr12":"XII",
-			"chr13":"XIII",
-			"chr14":"XIV",
-			"chr15":"XV",
-			"chr16":"XVI",
-			"1":"I",
-			"2":"II",
-			"3":"III", 
-			"4":"IV",
-			"5":"V",
-			"6":"VI",
-			"7":"VII",
-			"8":"VIII",
-			"9":"IX",
-			"10":"X",
-			"11":"XI",
-			"12":"XII",
-			"13":"XIII",
-			"14":"XIV",
-			"15":"XV",
-			"16":"XVI"}
+def changeChromes(chrom):
+	##sometimes input files names chromosomes differently, returns roman numeral chromosome names
+	chromDict = {"chr01":"I",
+				"chr02":"II",
+				"chr03":"III", 
+				"chr04":"IV",
+				"chr05":"V",
+				"chr06":"VI",
+				"chr07":"VII",
+				"chr08":"VIII",
+				"chr09":"IX",
+				"chr10":"X",
+				"chr11":"XI",
+				"chr12":"XII",
+				"chr13":"XIII",
+				"chr14":"XIV",
+				"chr15":"XV",
+				"chr16":"XVI",
+				"1":"I",
+				"2":"II",
+				"3":"III", 
+				"4":"IV",
+				"5":"V",
+				"6":"VI",
+				"7":"VII",
+				"8":"VIII",
+				"9":"IX",
+				"10":"X",
+				"11":"XI",
+				"12":"XII",
+				"13":"XIII",
+				"14":"XIV",
+				"15":"XV",
+				"16":"XVI"}
+	return chromDict[chrom]
 
-pughFile = open(sys.argv[1], 'r')
+def changePugh(file,output, head=True):
+	##combines forward and reverse reads into a total and outputs file
+	if head==True: header = file.readline() #get rid of header
+	assert len(header.split()) in range(3, 5)
+	for line in file:
+		listline = line.rstrip().split('\t')
+		total = float(listline[2])+float(listline[3])
+		output.write("%s\t%s\t%s\n" % (changeChromes(listline[0]), listline[1], total))
 
-pughFile.readline() #get rid of header
+def main():
+	pughFile = open(sys.argv[1])
+	output = open(sys.argv[2], 'w')
+	changePugh(pughFile, output, head=True)
 
-of = open("isw2_nucleosomes.sgr", 'w')
-
-for line in pughFile:
-	listline = line.rstrip().split('\t')
-	total = float(listline[2])+float(listline[3])
-	of.write("%s\t%s\t%s\n" % (chromDict[listline[0]], listline[1], total))
+if __name__ == '__main__':
+	main()
